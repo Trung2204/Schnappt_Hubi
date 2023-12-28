5e099d4b57c614cb9f897875d2c39c0d1b62e9f4
Trung2204 <minhtrung9a8lhpqn@gmail.com>
2023-12-28 23:15:45 +0100

    Add comment to code

--- Player.java
+++ Player.java
@@ -1,25 +1,41 @@
 package Main;
 
+// This class is used for create player's instances
 public class Player {
 
+	// Position of player
 	private int x;
 	private int y;
+	// Character of player (Rabbit or Mouse)
 	private String character;
+	// Number of actions of one player
 	private int numberOfActions;
+	// Current cell of player
 	private GridCell currentPosition;
 	
+	// Set value of x
 	public void setX(int x) { this.x = x; }
+	// Set value of y
 	public void setY(int y) { this.y = y; }
+	// Set value of character
 	public void setCharacter(String character) { this.character = character; }
+	// Set value of number of actions
 	public void setNumberOfAction() { this.numberOfActions = Compass.fieldType.getAction(); }
+	// Set current cell of player
 	public void setCurrentPosition() { this.currentPosition = Board.gridCells[x][y]; }
 	
+	// Get player's x
 	public int getX() {return x;}
+	// Get player's y
 	public int getY() {return y;}
+	// Get player's character
 	public String getCharacter() {return character;}
+	// Get player's number of actions
 	public int getNumberOfAction() {return numberOfActions;}
+	// Get player's current cell
 	public GridCell getCurrentPosition() { return currentPosition; }
 	
+	// Move player up
 	public void moveUp() {
 		int temp = x;
 		if (--temp >= 0) { 
@@ -32,6 +48,7 @@ public class Player {
 		}
 		else System.out.println("Cannot move out of board!");
 	}
+	// Move player down
 	public void moveDown() {
 		int temp = x;
 		if (++temp <= Board.size) { 
@@ -44,8 +61,8 @@ public class Player {
 		}
 		else System.out.println("Cannot move out of board!");
 	}
+	// Move player left
 	public void moveLeft()  {
-		
 		int temp = y;
 		if (--temp <= Board.size) { 
 			temp = y--;
@@ -57,6 +74,7 @@ public class Player {
 		}
 		else System.out.println("Cannot move out of board!");
 	}
+	// Move player right
 	public void moveRight() {
 		int temp = y;
 		if (++temp <= Board.size) { 
@@ -70,10 +88,11 @@ public class Player {
 		else System.out.println("Cannot move out of board!");
 	}
 	
-	
+	// Player can view curtain
 	public void viewCurtain(int x, int y) {
 		currentPosition.changeWallType();
 	}
+	// Player can view token
 	public void viewToken(int x, int y) {
 		currentPosition.flipToken();
 	}

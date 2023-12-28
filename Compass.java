208d2487787f1fdd40788d5e3be8ecc1f7849ff7
TheIncompetentHMT <130627808+TheIncompetentHMT@users.noreply.github.com>
2023-12-28 23:13:20 +0100

    Add files via upload

--- Compass.java
+++ Compass.java
@@ -2,97 +2,120 @@ package Main;
 import java.util.Random;
 
 public class Compass {
+// This class represents a compass that generates random field types
 
-//    protected static FieldType[] fields;
-    protected static FieldType fieldType;
-    // private String[] fields = {"clock", "two", "three", "clock", "ghost"};
-    // private int[] numberOfAction = {1, 2, 3, 1, 1};
-    private static Clock clock;
-    private static Random random = new Random();
 
-//    Compass()   {
-//        fields = FieldType.values();
-//    };
+    protected static FieldType fieldType;       // Protected static variable 'fieldType' to store the current field type
+    private static Clock clock;         // Private static variable 'clock' to store the associated clock object
+    private static Random random = new Random();        // Private static variable 'clock' to store the associated clock object
 
+    // Method to set the associated clock object
     public void setClock(Clock clock) {
-        Compass.clock = clock;
+        Compass.clock = clock;      // Assign the provided clock object to 'clock'
     }
 
-    public void spin() {
-        int angle = random.nextInt(360);
 
-        if (angle <= 72) { // CLOCK1
-            Compass.fieldType = FieldType.CLOCK1;
-        } else if (73 <= angle && angle <= 126) { // TWO
-        	Compass.fieldType = FieldType.TWO;
-        } else if (127 <= angle && angle <= 180) { // THREE
-        	Compass.fieldType = FieldType.THREE;
-        } else if (181 <= angle && angle <= 252) { // CLOCK2
-        	Compass.fieldType = FieldType.CLOCK2;
-        } else { // GHOST
-        	Compass.fieldType = FieldType.GHOST;
+    // Method to simulate the rotation of the compass
+    public void spin() {
+        
+        int angle = random.nextInt(360);        // Generate a random angle between 0 and 360 degrees
+
+        if (angle <= 72) {
+            Compass.fieldType = FieldType.CLOCK1;       // Field type CLOCK1
+        } else if (73 <= angle && angle <= 126) {
+        	Compass.fieldType = FieldType.TWO;          // Field type TWO
+        } else if (127 <= angle && angle <= 180) {
+        	Compass.fieldType = FieldType.THREE;        // Field type THREE
+        } else if (181 <= angle && angle <= 252) {
+        	Compass.fieldType = FieldType.CLOCK2;       // Field type CLOCK2
+        } else {
+        	Compass.fieldType = FieldType.GHOST;        // Field type GHOST
         }
     }
     
+
+    // Method to retrieve the current field type
     public FieldType getFieldType() {
-    	return fieldType;
+    	return fieldType;       // Return the current field type
     }
+
+
+    // Method to get the number of actions associated with the current field type
     public int getNumberOfAction() {
-        return fieldType.getAction();
+        return fieldType.getAction();       // Get the action value from the field type enum
     }
 
+
+    // Method to retrieve the associated clock object
     public Clock getClock() {
-        return clock;
+        return clock;       // Return the 'clock' reference
     }
 
+
+    // Method to update the clock's time based on the current field type
     public FieldType updateClock() {
-//        FieldType fieldType = this.spin();
-        clock.increaseTime(fieldType);
-        return fieldType;
+        clock.increaseTime(fieldType);      // Increase the clock's time
+        return fieldType;       // Return the updated field type
     }
     
+
+    // Method to check if the current field type is GHOST
     public boolean checkGhostField() {
     	if (fieldType == FieldType.GHOST)
-    		return true;
-    	return false;
+    		return true;        // The field type is GHOST      
+    	return false;       // The field type is not GHOST
     }
+
+    // Methods to check if the ghost can move in a specific direction   
     public boolean checkGhostMoveUp() {
-    	int temp = Board.getGhostX();
-    	if (--temp >= 0) return true;
-		return false;
+    	int temp = Board.getGhostX();       // Get the current ghost's x-coordinate
+    	if (--temp >= 0)        // Check if moving up is possible
+            return true;       // Movement allowed
+		return false;       // Movement not allowed
     }
+
+
     public boolean checkGhostMoveDown() {
-    	int temp = Board.getGhostX();
-    	if (++temp <= Board.size) return true;
-		return false;
+    	int temp = Board.getGhostX();       // Get the current ghost's x-coordinate
+    	if (++temp <= Board.size)       // Check if moving down is possible
+            return true;        // Movement allowed
+		return false;           // Movement not allowed
     }
+
+
     public boolean checkGhostMoveLeft() {
-    	int temp = Board.getGhostY();
-    	if (--temp >= 0) return true;
-		return false;
+    	int temp = Board.getGhostY();       // Get the current ghost's y-coordinate
+    	if (--temp >= 0)        // Check if moving left is possible
+            return true;        // Movement allowed
+		return false;       // Movement not allowed
     }
+
+
     public boolean checkGhostMoveRight() {
-    	int temp = Board.getGhostX();
-    	if (++temp <= Board.size) return true;
-		return false;
+    	int temp = Board.getGhostX();       // Get the current ghost's y-coordinate
+    	if (++temp <= Board.size)       // Check if moving right is possible
+            return true;        // Movement allowed
+		return false;       // Movement not allowed
     }
+
+
+    // Method to move the ghost based on the field type and ghost's location
     public void moveGhost() {
-    	if (checkGhostField()) {
-    		if (GridCell.isGhostFound) {
-    			
-    			
-    			
-    			if (checkGhostMoveUp()) {
-    				GridCell.swapToken(Board.getGhostCell(), Board.getAdjacentCellUp());
-    			}
-    			
-    			
-    			
-    			
-    			
-    			
-    		}
-    			
-    	}
+
+        // If the current field type is GHOST (indicating the presence of a ghost)
+        if (checkGhostField()) {
+
+            // Check if the ghost has been found (either by the player or by the compass)
+            if (GridCell.isGhostFound) {
+
+                // Check if the ghost can move up
+                if (checkGhostMoveUp()) {
+
+                    // Swap the positions of the ghost cell (Board.getGhostCell()) and the adjacent cell up (Board.getAdjacentCellUp())
+                    GridCell.swapToken(Board.getGhostCell(), Board.getAdjacentCellUp());
+                }
+            }
+        }
     }
+
 }
--- FieldType.java
+++ FieldType.java
@@ -1,19 +1,23 @@
 package Main;
 public enum FieldType {
+    // Enumeration representing the different field types
 
-    CLOCK1(1),
-    CLOCK2(1),
-    GHOST(1),
-    TWO(2),
-    THREE(3);
+    CLOCK1(1),  // Field Clock with 1 action
+    CLOCK2(1),  // Field Clock with 2 actions
+    GHOST(1),   // Field Ghost with 1 action
+    TWO(2),    // Field Two with 2 actions
+    THREE(3);   // Field Three with 3 actions
 
+    // Private attribute to store the action associated with each field type
     private final int action;
 
+    // Constructor to initialize the action value
     FieldType(int action) {
-        this.action = action;
+        this.action = action;       // Assign the provided action value to the 'action' attribute
     }
 
+    // Getter method to retrieve the action associated with the current field type
     public int getAction() {
-        return action;
+        return action;      // Return the 'action' attribute representing the number of actions for the current field
     }
 }

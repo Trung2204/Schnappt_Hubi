47b8a7ac14ccf14c799213147f3922f2288470ea
Phu <154642828+PhuNguyenPT@users.noreply.github.com>
2024-01-04 20:01:53 +0100

    Change to Main, Player, Direction->DirectionType

---
+++ Console_Game/src/main/MagicDoor.java
@@ -0,0 +1,23 @@
+package main;
+
+public class MagicDoor extends GridCell{
+	
+	public MagicDoor() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected static int x = -1;
+	protected static int y = -1;
+	
+	public static void setX(int x) { MagicDoor.x = x; }
+	public static void setY(int y) { MagicDoor.y = y; }
+	
+	public static int getX() { return MagicDoor.x; }
+	public static int getY() { return MagicDoor.y; }
+	
+	public static void updateMagicDoor(int x, int y) { 
+		MagicDoor.setX(x); 
+		MagicDoor.setY(y); 
+	} 
+}
